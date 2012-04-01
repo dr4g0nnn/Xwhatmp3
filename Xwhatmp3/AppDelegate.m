1b06ae2dc72defa7c4ea70300fab5d98d4eaf9ae
dr4g0n <dr4g0n@v0lu.me>
2012-04-01 22:12:03 +0100

    Added default preferences.

--- Xwhatmp3/AppDelegate.m
+++ Xwhatmp3/AppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "AppDelegate.h"
 
+#define NYES ([NSNumber numberWithBool:YES])
+#define NNO ([NSNumber numberWithBool:NO])
+
 @implementation AppDelegate
 
 @synthesize window = _window;
@@ -16,6 +19,31 @@
 @synthesize removeButton = _removeButton;
 @synthesize directoryTable = _directoryTable;
 @synthesize preferencesController;
+
++ (void)initialize {
+    NSMutableDictionary *initialValues = [NSMutableDictionary dictionary];
+
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"convert320"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"convertV0"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"convertV2"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"convertQ8"];
+    [initialValues setObject:[NSNumber numberWithBool:NO] forKey:@"convertAlac"];
+    [initialValues setObject:[NSNumber numberWithBool:NO] forKey:@"convertAac"];
+    [initialValues setObject:[NSNumber numberWithBool:NO] forKey:@"convertFlac"];
+    [initialValues setObject:[NSNumber numberWithBool:NO] forKey:@"createFlacTorrent"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"createTorrents"];
+    [initialValues setObject:[NSNumber numberWithBool:NO] forKey:@"dither"];
+    [initialValues setObject:[NSNumber numberWithBool:NO] forKey:@"replayGain"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"zeropad"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"moveLog"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"moveCue"];
+    [initialValues setObject:[NSNumber numberWithBool:YES] forKey:@"moveOther"];
+    [initialValues setObject:@"http://tracker.what.cd:34000/" forKey:@"tracker"];
+    [initialValues setObject:@"" forKey:@"passkey"];
+    [initialValues setObject:[NSNumber numberWithInt:1] forKey:@"threads"];
+
+    [[NSUserDefaultsController sharedUserDefaultsController] setInitialValues:initialValues];
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
