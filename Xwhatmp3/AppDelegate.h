f13540508dce192ed2a778aaa2af6a72856acf96
dr4g0n <dr4g0n@v0lu.me>
2012-04-01 16:14:59 +0100

    Initial Commit

---
+++ Xwhatmp3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Xwhatmp3
+//
+//  Created by dr4g0n on 01/04/2012.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Xwhatmp3/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  Xwhatmp3
+//
+//  Created by dr4g0n on 01/04/2012.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
